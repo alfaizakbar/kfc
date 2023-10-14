d9ef48833e0b221de4e1839d9b69a1157edc156c
alfaizakbar <alfaizakbart9@gmail.com>
2023-10-14 15:35:47 +0700

    first commit

---
+++ pagehome/store.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Store KFC</title>
+    <link rel="shortcut icon" href="img/logokfc.png" type="image/x-icon">
+    <link rel="stylesheet" href="store.css">
+    <style>
+        <?php include 'header.css'; ?>
+        <?php include 'header.php'; ?>
+
+
+        </style>
+
+</head>
+<body>
+    <div class="blog">
+        </div>
+    
+    <div class="contactt">
+            <div class="location">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15894.058295118291!2d97.1423721!3d5.1814675!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30478391dd6c9899%3A0xa5eb339df492ba09!2sKFC%20Lhokseumawe!5e0!3m2!1sid!2sid!4v1691497440960!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>            
+                </div>
+        <div class="hubungi">
+        
+        <div class="kfc">
+            <img src="img/kfclhokseumawe.jpg" alt="">
+        </div>
+       
+        <div class="alamat">
+            <h1>Franchise restoran yang terkenal dengan ember ayam goreng, dan hidangan kombo, serta hidangan sampingan.
+            </br/>
+            </br/>
+Opsi layanan: Makan di tempat · Bawa pulang · Antar tanpa bertemu
+            </br/>
+            </br/>
+Alamat: DEPAN ISLAMIC CENTRE, Jl. Merdeka No.25, Simpang Empat, Kec. Banda Sakti, Kabupaten Aceh Utara, Aceh 24355
+            </br/>
+            </br/>
+            Jam: 
+            Buka 24 jam
+        </br/>
+        </br/>         
+Telepon: (021) 14022</h1>
+  
+        </div>
+        </div>
+    </div>
+</body>
+<?php include "footer.php"; ?>
+
+</html> 
