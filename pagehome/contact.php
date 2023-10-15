71ead13fefd1192556192c8c1d8ffffa622dc798
amirullah310 <amirullah.lsm99@gmail.com>
2023-10-15 11:01:56 +0700

    contact blom siap

---
+++ pagehome/contact.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="contact.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+</head>
+<body>
+<?php include 'header3.php'; ?>
+    <!-- loader -->
+    <!-- <div class="bg-loader">
+        <div class="loader"></div>
+    </div> -->
+    <!-- header -->
+    <!-- <div class="medsos">
+        <div class="container">
+            <ul>
+                <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa-brands fa-youtube"></i></a></li>
+                <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+            </ul>
+        </div>
+    </div>
+    <header>
+     
+            <div class="container">
+
+                <h1><a href="index.html">AMIRULLAH</a></h1>
+                <ul>
+                    <li ><a href="index.html">HOME</a></li>
+                    <li><a href="about.html">ABOUT</a></li>
+                    <li ><a href="service.html">SERVICE</a></li>
+                    <li class="active"><a href="contact.html">CONTACT</a></li>
+                </ul>
+            </div>
+    </header>
+       label
+       <section class="label">
+        <div class="container">
+            <p>Home / Contact</p>
+        </div>
+    </section> -->
+   
+    <!-- service -->
+    <section class="service2">
+        <div class="container">
+        <h3>CONTACT INFO</h3>
+        <div class="box">
+            <div class="col-4">
+                    <h4>Address</h4>
+                    <p>Jl. Tgk Chiek Dialue, Meunasah Kumbang, Lhokseumawe , Aceh</p>
+                </div>
+                <div class="col-4">
+                    <h4>Email</h4>
+                    <p>amirullah.lsm992gmail.com</p>
+                </div>
+                <div class="col-4">
+                    <h4>Address</h4>
+                    <p>088764287243</p>
+                </div>
+                <div class="col-4">
+                    <h4>Address</h4>
+                    <p>089604232229</p>
+                </div>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3973.938008256895!2d97.15870671471144!3d5.1136940462887885!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x304783e89d451885%3A0xfa566d211aaa0d46!2sKumbang%20Punteut%2C%20Kec.%20Blang%20Mangat%2C%20Kota%20Lhokseumawe%2C%20Aceh!5e0!3m2!1sid!2sid!4v1672103998538!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                
+        </div> 
+        </div>
+    </section>
+    
+
+    <!-- <script type="text/javascript">
+        $(document).ready(function(){
+            $(".bg-loader").hide();
+        }) -->
+    <!-- </script> -->
+    <?php include 'footer2.php'; ?>
+</body>
+</html>
