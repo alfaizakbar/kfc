e9e50255dc82d17c2a6c85db78d685d7db7f9c16
amirullah310 <amirullah.lsm99@gmail.com>
2023-10-16 20:48:21 +0700

    hlmn contact

---
+++ pagehome/store2.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="store2.css">
+</head>
+<body>
+<?php include 'header3.php'; ?>
+
+    <div class="contactUs">
+        <div class="title">
+            <h2>CONTACT</h2>
+        </div>
+        <div class="box">
+            <div class="contact form">
+                <h3>Send a message</h3>
+                <form action="">
+                    <div class="formBox">
+                        <div class="row50">
+                            <div class="inputBox">
+                                <span>First Name</span>
+                                <input type="text" name="" placeholder="who?" id="">
+                            </div>
+                            <div class="inputBox">
+                                <span>Last Name</span>
+                                <input type="text" name="" placeholder="who?" id="">
+                            </div>
+                        </div>
+                        <div class="row50">
+                            <div class="inputBox">
+                                <span>Email Address</span>
+                                <input type="text" name="" placeholder="who?" id="">
+                            </div>
+                            <div class="inputBox">
+                                <span>Your Phone </span>
+                                <input type="text" name="" placeholder="who?" id="">
+                            </div>
+                        </div>
+                        <div class="row100">
+                            <div class="inputBox">
+                                <span>Message</span>
+                                <textarea placeholder="write your message" name="" id="" cols="30" rows="10"></textarea>
+                            </div>
+                            </div>
+                        <div class="row100">
+                            <div class="inputBox">
+                              <input type="submit" value="Send">
+                            </div>
+                            </div>
+                    </div>
+                </form>
+            </div>
+            <div class="contact info">
+                <h3>Contact Info</h3>
+                <div class="infoBox">
+                    <div class="">
+                        <span><ion-icon name="location"></ion-icon></span>
+                        <p>Lhokseumawe , Aceh <br> Indonesia</p>
+                </div>
+                    <div class="">
+                        <span><ion-icon name="mail"></ion-icon></span>
+                        <a href="mailto:amirullah.lsm99@gmail.com">alamir@gmail.com</a>
+                </div>
+                    <div class="">
+                        <span><ion-icon name="call"></ion-icon></span>
+                        <a href="tel:089604232229">0896 0423 2229</a>
+                </div>
+                <ul class="sci">
+                    <li><a href="#"><ion-icon name="logo-facebook"></ion-icon></a></li>
+                    <li><a href="#"><ion-icon name="logo-whatsapp"></ion-icon></a></li>
+                    <li><a href="#"><ion-icon name="logo-instagram"></ion-icon></a></li>
+                    <li><a href="#"><ion-icon name="logo-twitter"></ion-icon></a></li>
+                </ul>
+                </div>
+
+            </div>
+            <div class="contact map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3973.429699235792!2d97.13817807483109!3d5.194946137175071!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30479cd7307bc8c1%3A0x73fdb3e3ae800a71!2sSMK%20Negeri%201%20Lhokseumawe!5e0!3m2!1sid!2sid!4v1697462455362!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+    </div>
+    <?php include 'footer2.php'; ?>
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
